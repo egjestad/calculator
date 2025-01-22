592eee2d341b2aeff74362ac64f3782b8fa9f9b2
egjestad <egjestad@gmail.com>
2025-01-22 14:54:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import {CalculatorView} from '/src/views/calculatorView.vue'
+
+</script>
+
+<template>
+  <div id="app">
+  <header>
+
+    <div class="wrapper">
+      Calculator
+    </div>
+  </header>
+    <div id="calculator">
+      <CalculatorView />
+    </div>
+
+
+  </div>
+</template>
+
+<style>
+</style>
