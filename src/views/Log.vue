623e74bc753968b69f23e5cee3301c04d83df413
egjestad <egjestad@gmail.com>
2025-01-23 21:06:01 +0100

    log container

---
+++ src/views/Log.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="logContainer">
+    <div id="logTitle">
+      <label>Log</label>
+    </div>
+    <div class="calculationLog">
+
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+#logTitle {
+  font-size: x-large;
+  font-weight: bold;
+  margin-bottom: 20px;
+  font-family: Copperplate, times, serif;
+  color : #000000;
+}
+
+</style>
